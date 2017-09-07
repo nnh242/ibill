a71fad589f061ff27fdaa029087d67f719789f6d
Ngoc Tina Hoang <ngoc.tina.hoang@outlook.com>
2017-09-07 19:54:15 -0400

    initial files

---
+++ test/test-server.js
@@ -0,0 +1,23 @@
+const mocha = require ('mocha')
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server.js');
+
+const should = chai.should();
+const app = server.app;
+
+
+chai.use(chaiHttp);
+
+
+describe('index page', function() {
+  it('exists', function(done) {
+    chai.request(app)
+      .get('/')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
+    });
+  });
+});
