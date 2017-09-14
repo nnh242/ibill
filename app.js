e6c46acabb435929e9c2ccb9e268b9713411ad13
Ngoc Tina Hoang <ngoc.tina.hoang@outlook.com>
2017-09-14 14:36:23 -0400

    added mock API

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const MOCK_INVOICES = {
             "id" : "0001",
             "customer": "Lala Doe",
             "description": "HTML update service",
-            "price": "30.00",
+            "price": "30",
             "quantity": "2",
             "date": "2017-09-14"
         },
@@ -12,7 +12,7 @@ const MOCK_INVOICES = {
             "id" : "0002",
             "customer": "Jon Doe",
             "description": "CSS update service",
-            "price": "32.00",
+            "price": "32",
             "quantity": "4",
             "date": "2017-09-15"
         },
@@ -20,23 +20,34 @@ const MOCK_INVOICES = {
             "id" : "0003",
             "customer": "Mimi Doh",
             "description": "CSS update service",
-            "price": "31.00",
+            "price": "31",
             "quantity": "6",
             "date": "2017-09-14"
         }
     ]
 }
-
+//note: only this getInvoices function changes when we have a real API
 function getInvoices(callback) {
     setTimeout(function(){callback(MOCK_INVOICES)},100);
 }
 
 function displayInvoices (data) {
     for (index in data.allInvoices) {
-        $('body').append( '<p>' + data.allInvoices[index].text + '<p>');
+        let total = data.allInvoices[index].price * data.allInvoices[index].quantity;
+        console.log(total);
+        $('#invoices').append( 
+            '<p>' +'Invoice ID: '+ data.allInvoices[index].id + '<p>' +
+            '<p>' + 'Invoice Date:' + data.allInvoices[index].date + '<p>'+
+            '<p>' + 'Customer: ' + data.allInvoices[index].customer + '<p>'+ 
+            '<p>' + 'Description: '+ data.allInvoices[index].description + '<p>'+ 
+            '<p>' + 'Price: $' + data.allInvoices[index].price + '<p>'+ 
+            '<p>' + 'Quantity: ' + data.allInvoices[index].quantity + '<p>'+
+            '<p>' + 'Total Amount: $' + total + '<p>'
+        );
     }
 }
 
-$(document).ready(function (){
+$(document).ready(function() {
     getInvoices(displayInvoices);
-});
+})
+
