496fc687a7c43efe18002dc7a18b048108d143b9
Ngoc Tina Hoang <ngoc.tina.hoang@outlook.com>
2017-09-23 14:03:23 -0400

    building server

--- app.js
+++ app.js
@@ -21,30 +21,9 @@ function signInFn () {
         window.location.href = "dashboard.html";
     }
 }
+
 function registerFn () {
     $('#sign-in-section').addClass('hidden');
     $('#register-section').removeClass('hidden');
 }
-//note: only this getInvoices function changes when we have a real API
-function getInvoices(callback) {
-    setTimeout(function(){callback(MOCK_INVOICES)},100);
-}
-
-function displayInvoices (data) {
-    for (index in data.allInvoices) {
-        let total = data.allInvoices[index].price * data.allInvoices[index].quantity;
-        console.log(total);
-        $('#invoices').append( 
-            '<p>' +'Invoice ID: '+ data.allInvoices[index].id + '<p>' +
-            '<p>' + 'Invoice Date:' + data.allInvoices[index].date + '<p>'+
-            '<p>' + 'Customer: ' + data.allInvoices[index].customer + '<p>'+ 
-            '<p>' + 'Description: '+ data.allInvoices[index].description + '<p>'+ 
-            '<p>' + 'Price: $' + data.allInvoices[index].price + '<p>'+ 
-            '<p>' + 'Quantity: ' + data.allInvoices[index].quantity + '<p>'+
-            '<p>' + 'Total Amount: $' + total + '<p>'
-        );
-    }
-}
-
-
 
--- server.js
+++ server.js
@@ -1,5 +1,79 @@
 const express = require('express');
 const app = express();
+
 app.use(express.static('public'));
+//create
+
+//retrieve
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + 'index.html');
+});
+app.get('/login', (req,res) => {
+    res.sendFile(__dirname + 'login.html')
+});
+app.get('/dashboard', (req,res) => {
+    res.sendFile(__dirname + 'dashboard.html')
+});
+app.get('/preview', (req,res) => {
+    res.sendFile(__dirname + 'preview.html')
+} )
+
+//update
+
+//delete
+
+
 app.listen(process.env.PORT || 8080);
 exports.app = app;
+
+
+// catch-all endpoint if client makes request to non-existent endpoint
+app.use('*', function(req, res) {
+    res.status(404).json({message: 'Not Found'});
+  });
+
+let server;
+
+// this function connects to our database, then starts the server
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
+  });
+}
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
