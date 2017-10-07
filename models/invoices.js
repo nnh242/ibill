0e2b2f32be3434ad72350d69ccf6ac40c8e7aa09
nnh242 <nhoang@me.com>
2017-10-06 21:43:54 -0400

    added preview invoice page

--- models/invoices.js
+++ models/invoices.js
@@ -4,29 +4,19 @@ const invoiceSchema = mongoose.Schema ({
     number: {type: Number, required: true},
     date: {type: Date, required: false},
     customer: {type: String, required: true},
-    description: {type: String},
-    price: {type: Number, required: true},
-    quantity: {type: Number, required: true},
-    total: {type: Number}
+    item: {type: String},
+    price: {type: Number, required: true}
 });
 
-//this virtual for total amount is not tested
-invoiceSchema.virtual('totalAmt').get(function() {
-    const totalCalucated = (price,quantity) => {
-        return price*quantity;
-    }
-    return `$ ${totalCalculated}`;
-});
 
 invoiceSchema.methods.apiRepr = function() {
     return {
-      id: this.id,
+      id: this._id,
+      date: this.date,
       number: this.number,
       customer: this.customer,
-      description: this.description,
+      item: this.item,
       price: this.price,
-      quantity: this.quantity,
-      total: this.totalAmt
     };
   };
 const Invoice = mongoose.model('Invoice', invoiceSchema);
