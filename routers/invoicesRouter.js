23d7069ebb5287c29c5d13d5360b9d0bf7a4e9d0
nnh242 <nhoang@me.com>
2017-10-07 21:16:02 -0400

    integrating tests for mongoose endpoints

--- routers/invoicesRouter.js
+++ routers/invoicesRouter.js
@@ -9,15 +9,35 @@ mongoose.Promise = global.Promise;
 /* const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const jwtAuth = passport.authenticate('jwt', { session: false }); */
-//this is endpoint /api/invoices
+
 const requiredFields = ['number','customer','price', 'item'];
 
 const catchError = (err,res) => {
   console.error(err);
   return res.status(500).json({error: 'Something went wrong'});
 }
+//this is endpoint /api/invoices - getting all invoices
+router.get('/', (req, res) => {
+  Invoice
+    .find()
+    .then(invoices => {
+      res.json({
+        invoices: invoices.map(
+          (invoice) => invoice.apiRepr())
+      });
+    })
+    .catch(catchError);
+});
 
-router.post('/', jsonParser, (req,res) => {
+//this is endpoint gets a specific invoice by id
+router.get('/:id', (req, res) => {
+  Invoice
+  .findById(req.params.id)
+  .then(invoice => res.json(invoice.apiRepr()))
+  .catch(catchError);
+});
+
+router.post('/', (req,res) => {
   requiredFields.map((field) => {
     if (!(field in req.body)) {
       const message = `Missing \`${field}\` in request body`
@@ -40,55 +60,44 @@ router.post('/', jsonParser, (req,res) => {
 //how to write a get endpoint to retrieve all invoices for one user
 // how to add endpoint router/api/invoices/userA
 //how do i pass the user id into this endpoint? User.findOne({'userId':})
-router.get('/', (req, res) => {
-  Invoice
-    .find()
-    .limit(10)
-    .then(invoices => {
-      res.json({
-        invoices: invoices.map(
-          (invoice) => invoice.apiRepr())
-      });
-    })
-    .catch(catchError);
-});
-//this is endpoint gets a specific invoice by id
-router.get('/:id', (req, res) => {
-  Invoice
-  .findById(req.query.id)
-  .then(invoice => res.json(invoice.apiRepr()))
-  .catch(catchError);
-});
+
 
 //update invoice by id
-router.put('/:id', jsonParser, (req,res) => {
-  const findRequiredFields = [...requiredFields, 'id'];
+router.put('/:id', (req,res) => {
+  /* const findRequiredFields = [...requiredFields, 'id'];
   findRequiredFields.map(alertError => {
     if (!(field in body)) {
       const message = `Missing \`${field}\` in request body`
       console.error(message);
       return res.status(400).send(message);
+    } */
+    if (req.query.id !== req.body.id) {
+      const message = (
+        `Request path id (${req.params.id}) and request body id `
+        `(${req.body.id}) must match`);
+      console.error(message);
+      return res.status(400).send(message);
     }
-  });
-  if (req.query.id !== req.body.id) {
-    const message = (
-      `Request path id (${req.params.id}) and request body id `
-      `(${req.body.id}) must match`);
-    console.error(message);
-    return res.status(400).send(message);
-  }
-  console.log(`Updating invoice \`${req.params.id}\``);
-  Invoice.findByIdAndUpdate(req.params.id, {$set: toUpdate})
-  });
-  res.status(204).end();
-
+    const toUpdate = {};
+    const updateableFields = ['date','customer','item','price']
+    updateableFields.forEach(field => {
+      if (field in req.body) {
+        toUpdate[field] = req.body[field];
+      }
+    });
+    
+    Invoice
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .then(invoice =>  res.status(204).end())
+    .catch(catchError);
 });
 
 // delete invoice by id , tested successful
 router.delete('/:id', (req,res) => {
-    InvoicesList.delete(req.params.id);
-    console.log(`Deleting invoice with \`${req.params.id}\``);
-    res.status(204).end();
+    Invoice
+    .findByIdAndRemove(req.params.id)
+    .then(restaurant => res.status(204).end())
+    .catch(catchError)
 });
 
 module.exports = router;
