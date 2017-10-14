726a641bd11707250354e8461e8cf95bfbba05cf
nnh242 <nhoang@me.com>
2017-10-14 16:47:22 -0400

    wiring up client

--- public/client.js
+++ public/client.js
@@ -1,13 +1,10 @@
 
 $(document).ready(function() {
-    const email = $('#sign-in-email').val();
-    const password = $('#sign-in-password').val();
-
     $('#register-link').on('click', showRegisterFn);
     $('#sign-in-button').on('click', signInFn);
     //To Do: validate values from inputs username and password to sign in
     //To Do: validate values from the register form to register
-    //$('#register-button').on('submit', registerFn);
+    $('#register-button').on('click', registerFn);
     $('#form-button').on('click',showFormFn);
     $('#invoices').DataTable();
     
@@ -17,21 +14,55 @@ const catchAllError = err => {
           window.location = '/login.html';
         }
       };
-
+function registerFn() {
+    let username = $('#register-username').val();
+    let password = $('#register-password').val();
+    let company = $('#name').val();
+    let phone = $('#phone').val();
+    //let address = {street : $('#street').val(), }
+    let registerData = {username: username, password: password, company: company, phone: phone};
+    console.log (registerData);
+    // validate inputs
+    // no empty inputs
+    // phone must be number in phone format
+    // string fields must be string : name, street address, city
+    // make state to be a drop down
+    // zipcode must be zipcode
+    //username must be a string
+    // password must be at least 10 characters
+    $.ajax ({
+        url: '/api/users/register',
+        method: 'POST',
+        data: JSON.stringify(registerData),
+        dataType: 'json',
+        contentType:'application/json; charset=utf-8',
+        success: function(data) {
+            console.log(data);
+            console.log('user created successfully');
+            $('#register-form').empty();
+            $('#register-section').addClass('hidden');
+            $('#sign-in-section').removeClass('hidden');
+            $('#demo').append(`<span>Your account is registered, please sign in.</span>`)
+            $('#demo').removeClass('hidden');
+        },
+        error: catchAllError
+    })
+}
 //sign in function
-function signInFn(username, password) {
+function signInFn() {
+    let username = $('#sign-in-username').val();
+    let password = $('#sign-in-password').val();
     let userData ={ username: username, password: password};
-    console.log(userData);
     $.ajax({
         url: '/api/auth/login',
-        method: "GET",
+        method: 'GET',
         data: JSON.stringify(userData),
-        dataType: "json",
-        headers:  { 'Authorization': "Basic " + window.btoa(userData.username + ":" + userData.password) },
-        success: function(data){
-            console.log(data);
+        dataType: 'json',
+        headers:  { 'Authorization': 'Basic ' + window.btoa(userData.username + ':' + userData.password) },
+        success: function(token){
+            console.log(token);
             console.log('user is authenticated');
-            //now what to do to show that one authenticated user
+            //showDashBoardFn();
         },
         error: catchAllError
     })
@@ -43,6 +74,12 @@ function showFormFn() {
 
 function showRegisterFn() {
     $('#sign-in-section').addClass('hidden');
+    $('#demo').addClass('hidden');
     $('#register-section').removeClass('hidden');
 }
 
+/* function showDashBoardFn(){
+    $.ajax({
+        url:
+    })
+} */
